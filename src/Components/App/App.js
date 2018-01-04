c40572f1f48292145f563df13046bc076658d1ed
Kate McFaul <katemcfaul@gmail.com>
2018-01-03 16:14:11 -0800

    basic structure

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from '../Header/Header';
+import FavoriteProjects from '../FavoriteProjects/FavoriteProjects';
 
 class App extends Component {
   render() {
     return (
-      <p>Hello World</p>
+      <React.Fragment>
+        <Header />
+        <div id="wrapper">
+          <div id="content">
+            <FavoriteProjects />
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
---
+++ src/Components/FavoriteProjects/FavoriteProjects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const favoriteProjects = () => (
+  <div className="favoriteProjects">
+    <h2>Featured Projects</h2>
+  </div>
+);
+
+export default favoriteProjects;
