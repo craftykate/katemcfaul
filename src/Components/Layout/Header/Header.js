71419cd30fc584a4675e737a6d276c1aae43245b
Kate <katemcfaul@gmail.com>
2021-12-07 19:32:16 -0800

    Add Header

---
+++ src/Components/Layout/Header/Header.js
@@ -0,0 +1,55 @@
+// Packages
+import { NavLink } from 'react-router-dom'
+// CSS
+import './Header.modules.css'
+
+const Header = () => {
+  const links = [
+    {
+      title: 'Github',
+      link: 'https://github.com/craftykate',
+      linkName: 'github.com/craftykate',
+    },
+    {
+      title: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/katemcfaul/',
+      linkName: 'linkedin.com/in/katemcfaul',
+    },
+  ]
+
+  return (
+    <header>
+      <h1>KATE MCFAUL</h1>
+      <h2>PROGRAMMER, ARTIST, NERD</h2>
+
+      <ul>
+        {links.map((link) => (
+          <li>
+            {link.title}:{' '}
+            <a href={link.link} target='_blank' rel='noopener noreferrer'>
+              {link.linkName}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <nav>
+        <ul className='nav'>
+          <li>
+            <NavLink exact to='/'>
+              Featured Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/about'>About Kate</NavLink>
+          </li>
+          <li>
+            <NavLink to='/shop'>Shop</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
