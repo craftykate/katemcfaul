20a92d2002bddc75098aa7a581970cbe5941ef93
Kate <katemcfaul@gmail.com>
2021-12-11 18:09:47 -0800

    Add footer

---
+++ src/Components/Layout/Footer/Footer.js
@@ -0,0 +1,15 @@
+//CSS
+import './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>Site built by Kate McFaul</li>
+        <li>Admin</li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,11 +1,13 @@
 // Components
 import Header from './Header/Header'
+import Footer from './Footer/Footer'
 
 const Layout = ({ children }) => {
   return (
     <div id='wrapper'>
       <Header />
       <main>{children}</main>
+      <Footer />
     </div>
   )
 }
