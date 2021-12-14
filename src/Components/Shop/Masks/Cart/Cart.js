6bf9979cd448e8d5b6e7ad7b4a83ddd41530c0f9
Kate <katemcfaul@gmail.com>
2021-12-13 19:16:25 -0800

    Convert cart into modal and modal into portal

--- src/Components/Shop/Masks/Cart/Cart.js
+++ src/Components/Shop/Masks/Cart/Cart.js
@@ -4,8 +4,8 @@ import buttonClasses from 'Components/UI/Button/Button.module.css'
 // Components
 import CartRow from './CartRow'
 // UI
-import Card from 'Components/UI/Card/Card'
 import Button from 'Components/UI/Button/Button'
+import Modal from 'Components/UI/Modal/Modal'
 
 const Cart = ({
   setShowCart,
@@ -30,41 +30,39 @@ const Cart = ({
   let total = numItems * 20
   total = addStoppers ? (total += numItems) : total
   return (
-    <div className={classes.background} onClick={() => setShowCart(false)}>
-      <Card title='Cart' className={classes.display} onClick={doNotClose}>
-        <p>Screenshot cart and send to me so I know exactly what you want!</p>
-        {sortedItems.map((item, index) => {
-          return (
-            <CartRow
-              item={item}
-              adjustItemInCart={adjustItemInCart}
-              key={index}
-            />
-          )
-        })}
-        <div className={classes.totals}>
-          <p>Mask total: ${numItems * 20}</p>
-          <p>
-            <Button onClick={() => setAddStoppers(!addStoppers)}>
-              {stopperButtonText}
-            </Button>
-            Stoppers: {stopperAmount}
-          </p>
-          <p>Total: ${total}</p>
-        </div>
-        <div className={classes.actionButtons}>
-          <Button className={buttonClasses.big} onClick={emptyAndClose}>
-            Empty Cart
+    <Modal backgroundClose={() => setShowCart(false)} doNotClose={doNotClose}>
+      <p>Screenshot cart and send to me so I know exactly what you want!</p>
+      {sortedItems.map((item, index) => {
+        return (
+          <CartRow
+            item={item}
+            adjustItemInCart={adjustItemInCart}
+            key={index}
+          />
+        )
+      })}
+      <div className={classes.totals}>
+        <p>Mask total: ${numItems * 20}</p>
+        <p>
+          <Button onClick={() => setAddStoppers(!addStoppers)}>
+            {stopperButtonText}
           </Button>
-          <Button
-            className={buttonClasses.big}
-            onClick={() => setShowCart(false)}
-          >
-            Close
-          </Button>
-        </div>
-      </Card>
-    </div>
+          Stoppers: {stopperAmount}
+        </p>
+        <p>Total: ${total}</p>
+      </div>
+      <div className={classes.actionButtons}>
+        <Button className={buttonClasses.big} onClick={emptyAndClose}>
+          Empty Cart
+        </Button>
+        <Button
+          className={buttonClasses.big}
+          onClick={() => setShowCart(false)}
+        >
+          Close
+        </Button>
+      </div>
+    </Modal>
   )
 }
 
--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,5 +1,26 @@
-const Modal = () => {
-  return 'modal'
+// CSS
+import classes from './Modal.module.css'
+// Packages
+import ReactDOM from 'react-dom'
+// UI
+import Card from '../Card/Card'
+
+const ModalOverlay = ({ backgroundClose, doNotClose, children }) => {
+  return (
+    <div className={classes.background} onClick={backgroundClose}>
+      <Card title='Cart' className={classes.display} onClick={doNotClose}>
+        {children}
+      </Card>
+    </div>
+  )
+}
+const Modal = ({ backgroundClose, doNotClose, children }) => {
+  return ReactDOM.createPortal(
+    <ModalOverlay backgroundClose={backgroundClose} doNotClose={doNotClose}>
+      {children}
+    </ModalOverlay>,
+    document.getElementById('modal-root')
+  )
 }
 
 export default Modal
